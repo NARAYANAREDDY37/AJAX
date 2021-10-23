b06ff1443ed3674aca2a10f237418f2f32f4131a
narayanareddy <narayanareddychinnapureddy@yahoo.com>
2021-10-23 11:04:15 +0530

    employee portal axios done

---
+++ 09_Employee_Portal_Axios/app.js
@@ -0,0 +1,149 @@
+const serverURL = `http://127.0.0.1:3000/api`;
+
+//DOM Content Loaded (get request)
+
+window.addEventListener('DOMContentLoaded', () => {
+	fetchAllEmployees();
+});
+
+let fetchAllEmployees = () => {
+	let url = `${serverURL}/employees`;
+	axios.get(url).then((response) => {
+		let employees = response.data;
+		let employeeRows = '';
+		for (let employee of employees) {
+			employeeRows += `<tr>
+                                <td>${employee.id}</td>
+                                <td>${employee.first_name}</td>
+                                <td>${employee.last_name}</td>
+                                <td>${employee.email}</td>
+                                <td>${employee.gender}</td>
+                                <td>${employee.ip_address}</td>
+                                <td>
+                                        <button class="btn btn-secondary mt-0 btn-sm update form-inline-group">Update</button>
+                                        <button class="btn btn-danger mt-0 btn-sm delete form-inline-group">Delete</button>                                 
+                                </td>
+                            </tr>`;
+		}
+		document.querySelector('#table-body').innerHTML = employeeRows;
+	});
+};
+
+//Add employee form
+
+let addEmployeeForm = document.querySelector('#add-employee-form');
+addEmployeeForm.addEventListener('submit', (e) => {
+	e.preventDefault(); // Prevent auto form submission
+	$('#add-employee-modal').modal('hide'); //to automatically close the modal when you submit the form (jquery snnipet)
+
+	//fetching the form data
+	let newEmployee = {
+		first_name: document.querySelector('#add-first-name').value,
+		last_name: document.querySelector('#add-last-name').value,
+		email: document.querySelector('#add-email').value,
+		gender: document.querySelector('#add-gender').value,
+		ip_address: document.querySelector('#add-ip-address').value,
+	};
+
+	//now sending this dynamic object to the AJAX post request to store the data
+
+	let url = `${serverURL}/employees`;
+
+	axios
+		.post(url, newEmployee)
+		.then((response) => {
+			console.log(response.data);
+			fetchAllEmployees();
+			clearFormFields();
+		})
+		.catch((error) => {
+			console.log(error);
+		});
+});
+
+// clearing the form fields after adding employee
+
+let clearFormFields = () => {
+	(document.querySelector('#add-first-name').value = ''),
+		(document.querySelector('#add-last-name').value = ''),
+		(document.querySelector('#add-email').value = ''),
+		(document.querySelector('#add-gender').value = ''),
+		(document.querySelector('#add-ip-address').value = '');
+};
+
+// Deleting employee from table
+//Click event on entire table bodyParser
+
+let tableBody = document.querySelector('#table-body');
+tableBody.addEventListener('click', (e) => {
+	let targetElement = e.target;
+
+	//Click On Delete Button
+	if (targetElement.classList.contains('delete')) {
+		//getting the specific ID of that button when clicked
+		let selectedId =
+			targetElement.parentElement.parentElement.firstElementChild.innerHTML;
+		let url = `${serverURL}/employees/${selectedId}`;
+
+		axios
+			.delete(url)
+			.then((response) => {
+				console.log(response.data);
+				fetchAllEmployees();
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}
+
+	//Click On Update Button
+	if (targetElement.classList.contains('update')) {
+		let selectedId =
+			targetElement.parentElement.parentElement.firstElementChild.innerHTML;
+
+		let url = `${serverURL}/employees`;
+		axios.get(url).then((response) => {
+			let employees = response.data;
+			let selectedEmployee = employees.find((employee) => {
+				return employee.id === selectedId.trim();
+			});
+
+			//now we got the object of selected employee and we need to populate to the modal
+
+			populateUpdateModal(selectedEmployee); //fetching data into the modal
+		});
+	}
+});
+let populateUpdateModal = (selectedEmployee) => {
+	document.querySelector('#update-emp-id').value = selectedEmployee.id;
+	document.querySelector('#update-first-name').value =
+		selectedEmployee.first_name;
+	document.querySelector('#update-last-name').value =
+		selectedEmployee.last_name;
+	document.querySelector('#update-email').value = selectedEmployee.email;
+	document.querySelector('#update-gender').value = selectedEmployee.gender;
+	document.querySelector('#update-ip-address').value =
+		selectedEmployee.ip_address;
+	$('#update-employee-modal').modal('show'); //to show the modal when you click on update
+};
+
+//Update Form Submission
+let updateEmployeeForm = document.querySelector('#update-employee-form');
+updateEmployeeForm.addEventListener('submit', (e) => {
+	let employeeID = document.querySelector('#update-emp-id').value.trim();
+	e.preventDefault();
+	$('#update-employee-modal').modal('hide'); //to close the modal
+	let employee = {
+		first_name: document.querySelector('#update-first-name').value,
+		last_name: document.querySelector('#update-last-name').value,
+		email: document.querySelector('#update-email').value,
+		gender: document.querySelector('#update-gender').value,
+		ip_address: document.querySelector('#update-ip-address').value,
+	};
+	let url = `${serverURL}/employees/${employeeID}`;
+
+	axios.put(url, employee).then((response) => {
+		console.log(response.data);
+		fetchAllEmployees();
+	});
+});
